8a695a2248e3169d49c597722ca3c25517097dc0
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-08 21:25:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor :name, :song
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
@@ -7,6 +7,6 @@ class Artist
   end
 
   def add_song(song)
-    song.artist = self
+    artist.songs = self
   end
 end
